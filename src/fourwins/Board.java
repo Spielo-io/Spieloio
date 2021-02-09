398e066b01b7cde59d398062d60c00c709477c92
adrian dosch <A_Dosch@gmx.de>
2021-02-09 18:31:17 +0100

    implemented prototype of status

--- src/fourwins/Board.java
+++ src/fourwins/Board.java
@@ -1,5 +1,23 @@
 package fourwins;
 
 public class Board {
+	//public
 
+	Board(){
+		setGameStatus(status.IDLE);
+	}
+	
+	public status getGameStatus() {
+		return gameStatus;
+	}
+	
+	public void setGameStatus(status status) {
+		this.gameStatus = status;
+	}
+	
+	//private
+	private status gameStatus;
+	private enum status  {
+		IDLE, YOURTURN, OPPONENTTURN
+	}
 }
