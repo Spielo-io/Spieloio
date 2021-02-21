1e0db7c8ffb1aaf6fd6835e7c2c2c95726418100
michaelprokein <prokein.michae-it19@it.dhbw-ravensburg.de>
2021-02-21 18:25:08 +0100

    Added new LobbyScreens and removed HashList

--- src/Spielo.java
+++ src/Spielo.java
@@ -8,11 +8,22 @@ public class Spielo {
 	private JFrame frame;
 	private static CardLayout cardLayout;
 	private static Container container;
-	private static SpieloView currentView;
-	private static HashMap<String, SpieloView> screens;
+//	private static SpieloView currentView;
+//	private static HashMap<String, SpieloView> screens;
+
+	private static StartScreen startScreen;
+	private static LobbyCreateScreen lobbyCreateScreen;
+	private static LobbySelectScreen lobbySelectScreen;
+	private static LobbyScreenHostPublic lobbyScreenHostPublic;
+	private static LobbyScreenHostPrivat lobbyScreenHostPrivat;
+	private static LobbyScreenClientPublic lobbyScreenClientPublic;
+	private static LobbyScreenClientPrivat lobbyScreenClientPrivat;
+	private static GameScreen gameScreen;
+
+	private static LobbyScreen currentLobbyScreen;
+
 
 	private static boolean isHost;
-	private static boolean isPrivatLobby;
 
 	public static String username;
 
@@ -20,83 +31,98 @@ public class Spielo {
 		new Spielo();
 	}
 
-	public Spielo(){
+	public Spielo() {
 		initializeElements();
-		configureJFrame();
-		loadScreens();
+		configureElements();
 	}
 
-	private void initializeElements(){
+	private void initializeElements() {
 		frame = new JFrame();
 		container = frame.getContentPane();
 		cardLayout = new CardLayout();
-		screens = new HashMap<String, SpieloView>();
+//		screens = new HashMap<String, SpieloView>();
+
+		startScreen = new StartScreen();
+		lobbyCreateScreen = new LobbyCreateScreen();
+		lobbySelectScreen = new LobbySelectScreen();
+		lobbyScreenHostPublic = new LobbyScreenHostPublic();
+		lobbyScreenHostPrivat = new LobbyScreenHostPrivat();
+		lobbyScreenClientPublic = new LobbyScreenClientPublic();
+		lobbyScreenClientPrivat = new LobbyScreenClientPrivat();
+		gameScreen = new GameScreen();
 
 		isHost = false;
 	}
 
-	private void configureJFrame(){
+	private void configureElements() {
 
 		frame.setSize(700, 500);
+		frame.setLocation(300, 200);
 		frame.setTitle("Spielo.io");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 		frame.setLayout(cardLayout);
-//		frame.pack();
 
-		container.setPreferredSize(new Dimension(500, 300));
+		cardLayout.addLayoutComponent(startScreen, "StartScreen");
+		cardLayout.addLayoutComponent(lobbyCreateScreen, "LobbyCreateScreen");
+		cardLayout.addLayoutComponent(lobbySelectScreen, "LobbySelectScreen");
+		cardLayout.addLayoutComponent(lobbyScreenHostPublic, "LobbyScreenHostPublic");
+		cardLayout.addLayoutComponent(lobbyScreenHostPrivat, "LobbyScreenHostPrivat");
+		cardLayout.addLayoutComponent(lobbyScreenClientPublic, "LobbyScreenClientPublic");
+		cardLayout.addLayoutComponent(lobbyScreenClientPrivat, "LobbyScreenClientPrivat");
+		cardLayout.addLayoutComponent(gameScreen, "GameScreen");
+
+		frame.add(startScreen);
+		frame.add(lobbyCreateScreen);
+		frame.add(lobbySelectScreen);
+		frame.add(lobbyScreenHostPublic);
+		frame.add(lobbyScreenHostPrivat);
+		frame.add(lobbyScreenClientPublic);
+		frame.add(lobbyScreenClientPrivat);
+		frame.add(gameScreen);
 	}
 
-	public static boolean userIsHost(){
+	public static boolean userIsHost() {
 		return isHost;
 	}
 
-	public static void changeView(String newView){
-		currentView = screens.get(newView);
-//				get username from StartScreen
-		if(!newView.equals("StartScreen")){
-			username = ((StartScreen)screens.get("StartScreen")).getUsername();
-		}
-
+	public static void changeView(String newView) {
 		switch (newView) {
 			case "StartScreen" -> {
-				((StartScreen) currentView).clearJoinCodeTextfield();
+				startScreen.clearJoinCodeTextfield();
 				isHost = false;
 			}
 			case "LobbyCreateScreen" -> {
 				isHost = true;
 			}
-			case "LobbySelectScreen" -> {
-				isPrivatLobby = false;
+			case "LobbyScreenHostPublic" -> {
+				lobbyScreenHostPublic.setLobbySettings(lobbyCreateScreen.getLobbySettings(), true);
+				currentLobbyScreen = lobbyScreenHostPublic;
 			}
-			case "LobbyScreen" -> {
-				((LobbyScreen)currentView).lobbySettings_Panel.activateRadioButtons(isHost);
-				if(isHost){
-					((LobbyScreen)currentView).lobbySettings_Panel.setLobbySettings(((LobbyCreateScreen)screens.get("LobbyCreateScreen")).getLobbySettings(), isHost);
-
-				}
-				((LobbyScreen)currentView).lobbySettings_Panel.disableVisibiltyButtonGroupSetting();
-				((LobbyScreen)currentView).setStartGameButton(isHost);
+			case "LobbyScreenHostPrivat" -> {
+				lobbyScreenHostPrivat.setLobbySettings(lobbyCreateScreen.getLobbySettings(), true);
+				currentLobbyScreen = lobbyScreenHostPrivat;
+			}
+			case "LobbyScreenClientPublic" -> {
+				currentLobbyScreen = lobbyScreenClientPublic;
+//				currentLobbyScreen.setLobbySettings(new String[]{"Dame", "", "", ""}, isHost);
+			}
+			case "LobbyScreenClientPrivat" -> {
+				currentLobbyScreen = lobbyScreenClientPrivat;
 			}
 		}
+//			get and set username
+		if (!newView.equals("StartScreen")) {
+			username = startScreen.getUsername();
+		}
+
+		if(newView.contains("LobbyScreen")){
+			currentLobbyScreen.setNameForPlayerOne(username);
+		}
+
 //			actualize view
 		cardLayout.show(container, newView);
 	}
 
-	private void loadScreens(){
-		screens.put("StartScreen", new StartScreen());
-		screens.put("LobbyCreateScreen", new LobbyCreateScreen());
-		screens.put("LobbyScreen", new LobbyScreen());
-		screens.put("GameScreen", new GameScreen());
-		screens.put("LobbySelectScreen", new LobbySelectScreen());
-
-		SpieloView currentScreen;
-		for(String screen : screens.keySet()){
-			currentScreen = screens.get(screen);
-			cardLayout.addLayoutComponent(currentScreen, screen);
-			frame.add(currentScreen);
-			}
-		}
-
-	}
+}
 
